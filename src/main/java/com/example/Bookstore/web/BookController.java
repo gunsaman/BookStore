f50431edde4f38828b5cf9bdb2e2b0e371cfefba
Anil Lama <anil.lama@myy.haaga-helia.fi>
2020-09-06 23:38:35 +0300

    First commit

---
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -0,0 +1,16 @@
+package com.example.Bookstore.web;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+
+public class BookController {
+	
+		@RequestMapping("/index")
+		public String index() {
+			return "This is the main page";
+		}
+		
+	}
