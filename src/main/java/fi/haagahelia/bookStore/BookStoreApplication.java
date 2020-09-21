0bd81703e8e1e22a2887ee055fa166e62db17e16
Anil Lama <heaven.gunsa@gmail.com>
2020-09-21 09:21:20 +0300

    onetomany category_book

--- src/main/java/fi/haagahelia/bookStore/BookStoreApplication.java
+++ src/main/java/fi/haagahelia/bookStore/BookStoreApplication.java
@@ -1,13 +1,48 @@
 package fi.haagahelia.bookStore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import fi.haagahelia.bookStore.domain.Book;
+import fi.haagahelia.bookStore.domain.BookRepository;
+import fi.haagahelia.bookStore.domain.Category;
+import fi.haagahelia.bookStore.domain.CategoryRepository;
+
+
 
 @SpringBootApplication
 public class BookStoreApplication {
-
+	private static final Logger log = LoggerFactory.getLogger(BookStoreApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
 	}
+	
 
+	@Bean
+	public CommandLineRunner Book(BookRepository brepository, CategoryRepository repository2) {
+		return (args) -> {
+			
+			log.info("categories  of books");
+			repository2.save(new Category("Classics"));
+			repository2.save(new Category("Science fiction"));
+			repository2.save(new Category("History"));
+			
+			brepository.save(new Book("aa","john", 2020,"12457812", 29, repository2.findByName("Classics").get(0)));
+			brepository.save(new Book("zz","aman", 2020,"12457812", 29, repository2.findByName("Science fiction").get(0)));
+			
+				
+			
+			log.info("fetch all categories");
+			for (Category abc : repository2.findAll()) {
+				log.info(abc.toString());
+			}
+		};
+		}
+		
+	
+	
 }
--- src/main/java/fi/haagahelia/bookStore/domain/Book.java
+++ src/main/java/fi/haagahelia/bookStore/domain/Book.java
@@ -1,29 +1,50 @@
 package fi.haagahelia.bookStore.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 
 @Entity
 public class Book {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
 	private String title;
 	private String author;
 	private int year;
 	private String isbn;
 	private int price;
+	@ManyToOne
+	@JoinColumn(name ="categoryid")
+	private Category category;
 	
 	
 	public Book() {
-		super();
-		// TODO Auto-generated constructor stub
+		
 	}
+		
 
-
-	public Book(String title, String author, int year, String isbn, int price) {
+	public Book(String title, String author, int year, String isbn, int price, Category category) {
 		super();
 		this.title = title;
 		this.author = author;
 		this.year = year;
 		this.isbn = isbn;
 		this.price = price;
+		this.category = category;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
 	}
 
 
@@ -76,9 +97,30 @@ public class Book {
 		this.price = price;
 	}
 	
-	public String toString() {
-		return "Hello from book ";
-	}
 	
 
+	public Category getCategory() {
+		return category;
+	}
+
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+
+
+	@Override
+	public String toString() {
+		if(this.category != null)
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn
+				+ ", price=" + price + "category = "+this.getCategory()+" ]";
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn
+				+ ", price=" + price + "]";
+	}
+	
+	
 }
+
+
+	
